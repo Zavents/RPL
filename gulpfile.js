0e11f0f8e40a96c21d9183ed0751208d0136782a
zaven <pku.mca16@gmail.com>
2024-12-02 10:44:13 +0700

    lag as fuck anjgs

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,16 @@ const uglify = require("gulp-uglify");
 
 gulp.task("scripts", function () {
   return gulp
-    .src(["./public/js/aos.js", "./public/js/tw-ele.js", "./public/js/main.js", "./public/js/alpine.js", "./public/js/fancybox.js", "./public/js/swiper.js"])
+    .src([
+      "./public/js/aos.js",
+      "./public/js/tw-ele.js",
+      "./public/js/main.js",
+      "./public/js/alpine.js",
+      "./public/js/fancybox.js",
+      "./public/js/swiper.js",
+      "./public/js/bundle.min.js",
+      "./public/js/galeri.js"
+    ])
     .pipe(concat("bundle.min.js"))
     .pipe(uglify())
     .pipe(gulp.dest("./public/js/"));
